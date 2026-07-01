4f97eeaa834197f6bc900ea1503e22c6411bacb9
71104 <a71104@gmail.com>
2026-07-01 02:00:04 +0200

    Test `hash_to_scalar`.

--- src/utils.rs
+++ src/utils.rs
@@ -17,5 +17,19 @@ pub(crate) fn hash_to_scalar(message: &[u8]) -> Scalar {
 mod tests {
     use super::*;
 
-    // TODO
+    fn parse_scalar(s: &'static str) -> Scalar {
+        s.parse().unwrap()
+    }
+
+    #[test]
+    fn test_hash_to_scalar() {
+        assert_eq!(
+            hash_to_scalar(b"lorem ipsum dolor sit amet"),
+            parse_scalar("0x69c562c4b39c86fc322322c86cfe5be83fbd472c6a38862bdd2f362bfa442ad6")
+        );
+        assert_eq!(
+            hash_to_scalar(b"sator arepo tenet opera rotas"),
+            parse_scalar("0x027880d47636bf77d55804a6cf2d5ec8f09427cdf678e2ed3d74c432cc2efa7a")
+        );
+    }
 }
