3cf6128e664785d0136efce609d4d0be2cf05e92
Alberto La Rocca <71104@users.noreply.github.com>
2026-07-24 17:44:54 +0200

    Update PCS.

--- src/chip.rs
+++ src/chip.rs
@@ -6,12 +6,17 @@ use anyhow::Result;
 ///
 /// The generic parameter `I` is the number of inputs and `O` is the number of outputs.
 pub trait Chip<const I: usize, const O: usize> {
+    /// Returns the number of columns required by the chip.
+    fn width(&self) -> usize;
+
+    /// Builds the chip on the provided [`CircuitView`].
     fn build(
         &self,
         builder: &mut impl CircuitView,
         inputs: [Option<Cell>; I],
     ) -> Result<[Option<Cell>; O]>;
 
+    /// Witnesses execution of the chip in the provided [`WitnessView`].
     fn witness(
         &self,
         witness: &mut impl WitnessView,
