7b04bf78374d45383bdeb68fac6cf7ba5bed4db8
71104 <a71104@gmail.com>
2026-07-25 22:50:30 +0200

    Add tests for various `check_witness` failures.

--- src/plonk.rs
+++ src/plonk.rs
@@ -1721,4 +1721,90 @@ mod tests {
         assert!(test_vitalik_circuit_variation_impl::<Sha2Hash<Scalar>>(false, 2).is_ok());
         assert!(test_vitalik_circuit_variation_impl::<Sha2Hash<Scalar>>(true, 2).is_ok());
     }
+
+    fn build_vitalik_circuit() -> Circuit {
+        let mut builder = CircuitBuilder::default();
+        builder.add_gate(0, (var(0) ^ 2) - var(1));
+        builder.connect(cell(0, 0).into(), cell(1, 0).into());
+        builder.connect(cell(0, 1).into(), cell(1, 1).into());
+        builder.add_gate(1, var(0) * var(1) + var(0) + 5 - var(2));
+        builder.connect(cell(0, 0).into(), cell(2, 0).into());
+        builder.connect(cell(1, 2).into(), cell(2, 1).into());
+        builder.add_gate(2, Constraint::nop());
+        builder.declare_public_rows([2]);
+        builder
+            .build(CompilationOptions {
+                canonicalize_constraints: false,
+            })
+            .unwrap()
+    }
+
+    #[test]
+    fn test_check_witness_detects_wrong_number_of_rows() {
+        let circuit = build_vitalik_circuit();
+        let witness = Witness::new(4, 3, [0, 1]);
+        let error = circuit.check_witness(&witness).unwrap_err();
+        assert!(error.to_string().contains("wrong number of rows"));
+    }
+
+    #[test]
+    fn test_check_witness_detects_wrong_number_of_columns() {
+        let circuit = build_vitalik_circuit();
+        let witness = Witness::new(3, 4, [0, 1]);
+        let error = circuit.check_witness(&witness).unwrap_err();
+        assert!(error.to_string().contains("wrong number of columns"));
+    }
+
+    #[test]
+    fn test_check_witness_detects_wrong_degree_bound() {
+        let circuit = build_vitalik_circuit();
+        let witness = Witness::new(3, 3, [-2, -1, 0, 1, 2]);
+        let error = circuit.check_witness(&witness).unwrap_err();
+        assert!(error.to_string().contains("incorrect degree bound"));
+    }
+
+    #[test]
+    fn test_check_witness_detects_gate_constraint_violation() {
+        let circuit = build_vitalik_circuit();
+        let mut witness = circuit.make_witness();
+        witness.set(cell(0, 0), from_const(3));
+        witness.set(cell(0, 1), from_const(10));
+        witness.set(cell(1, 0), from_const(3));
+        witness.set(cell(1, 1), from_const(9));
+        witness.set(cell(1, 2), from_const(35));
+        witness.set(cell(2, 0), from_const(3));
+        witness.set(cell(2, 1), from_const(35));
+        let error = circuit.check_witness(&witness).unwrap_err();
+        assert!(error.to_string().contains("gate constraint"));
+    }
+
+    #[test]
+    fn test_check_witness_detects_direct_wire_constraint_violation() {
+        let circuit = build_vitalik_circuit();
+        let mut witness = circuit.make_witness();
+        witness.set(cell(0, 0), from_const(3));
+        witness.set(cell(0, 1), from_const(9));
+        witness.set(cell(1, 0), from_const(4));
+        witness.set(cell(1, 1), from_const(9));
+        witness.set(cell(1, 2), from_const(45));
+        witness.set(cell(2, 0), from_const(3));
+        witness.set(cell(2, 1), from_const(45));
+        let error = circuit.check_witness(&witness).unwrap_err();
+        assert!(error.to_string().contains("wire constraint violated"));
+    }
+
+    #[test]
+    fn test_check_witness_detects_transitive_wire_constraint_violation() {
+        let circuit = build_vitalik_circuit();
+        let mut witness = circuit.make_witness();
+        witness.set(cell(0, 0), from_const(3));
+        witness.set(cell(0, 1), from_const(9));
+        witness.set(cell(1, 0), from_const(3));
+        witness.set(cell(1, 1), from_const(9));
+        witness.set(cell(1, 2), from_const(35));
+        witness.set(cell(2, 0), from_const(99));
+        witness.set(cell(2, 1), from_const(35));
+        let error = circuit.check_witness(&witness).unwrap_err();
+        assert!(error.to_string().contains("wire constraint violated"));
+    }
 }
